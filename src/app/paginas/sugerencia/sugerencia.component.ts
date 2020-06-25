17da607668529996044ca37f967e468733b5cba2
ElMarto <martin@mata.com>
2020-06-24 21:38:19 -0300

    Agrega pantalla de sugerencias

--- src/app/paginas/sugerencia/sugerencia.component.ts
+++ src/app/paginas/sugerencia/sugerencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sugerencia',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SugerenciaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  volver() {
+    this.router.navigateByUrl('');
+  }
 }
